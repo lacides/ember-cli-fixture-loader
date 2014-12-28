6df160f689890c91b6423333cde4ee50b1cce46e
Lacides Charris <lcharris89@gmail.com>
2014-12-28 13:58:22 -0500

    Add blueprint for an empty fixture

---
+++ blueprints/fixture/files/app/fixtures/__name__.js
@@ -0,0 +1,14 @@
+/*
+export default [
+ {
+   id: 1,
+   name: 'Example'
+ },
+ {
+  id: 2,
+  name: 'Example 2'
+ }
+];
+*/
+
+export default [];
---
+++ blueprints/fixture/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  description: 'Generates an empty fixture for a model.'
+};
